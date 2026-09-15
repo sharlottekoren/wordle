dc651d63d2bc765bbba2c0e1117eb1e684466d7a
sharlottekoren <sharlottekoren@gmail.com>
2026-09-15 12:08:03 +0100

    created an on-screen keyboard

--- wordle.js
+++ wordle.js
@@ -31,37 +31,83 @@ function initialize() {
         }
     }
 
+    // Create the keyboard
+    let keyboard = [
+        ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", " "],
+        ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫"]
+    ];
+
+    for (let i = 0; i < keyboard.length; i++) {
+        let currRow = keyboard[i];
+        let keyboardRow = document.createElement("div");
+        keyboardRow.classList.add("keyboard-row");
+
+        for (let j = 0; j < currRow.length; j++) {
+            let keyTile = document.createElement("div");
+
+            let key = currRow[j];
+            keyTile.innerText = key;
+            if (key === "Enter") {
+                keyTile.id = "Enter";
+            }
+            else if (key == "⌫") {
+                keyTile.id = "Backspace";
+            }
+            else if ("A" <= key && key <= "Z") {
+                keyTile.id = "Key" + key;
+            }
+
+            keyTile.addEventListener("click", processKey);
+
+            if (key == "Enter") {
+                keyTile.classList.add("enter-key-tile");
+            } else {
+                keyTile.classList.add("key-tile");
+            }
+            keyboardRow.appendChild(keyTile);
+        }
+        document.body.appendChild(keyboardRow);
+    }
+
     // Listen for key press
     document.addEventListener("keyup", (e) => {
-        if (gameOver) return;
-
-        if ("KeyA" <= e.code && e.code <= "KeyZ") {
-            if (col < width) {
-                let currentTile = document.getElementById(row.toString() + "-" + col.toString());
-                if (currentTile.innerText === "") {
-                    currentTile.innerText = e.code[3];
-                    col += 1;
-                }
-            }
-        }
-        else if (e.code === "Backspace") {
-            if (0 < col && col <= width) {
-                col -= 1;
-            }
-            let currentTile = document.getElementById(row.toString() + "-" + col.toString());
-            currentTile.innerText = "";
-        }
-        else if (e.code === "Enter") {
-            update();
-            row += 1;
-            col = 0;
-        }
-
-        if (!gameOver && row === height) {
-            gameOver = true;
-            document.getElementById("answer").innerText = word;
-        }
+        processInput(e);
     });
+}
+
+function processKey() {
+    let e = { "code": this.id };
+    processInput(e);
+}
+
+function processInput(e) {
+    if (gameOver) return;
+
+    if ("KeyA" <= e.code && e.code <= "KeyZ") {
+        if (col < width) {
+            let currentTile = document.getElementById(row.toString() + "-" + col.toString());
+            if (currentTile.innerText === "") {
+                currentTile.innerText = e.code[3];
+                col += 1;
+            }
+        }
+    }
+    else if (e.code === "Backspace") {
+        if (0 < col && col <= width) {
+            col -= 1;
+        }
+        let currentTile = document.getElementById(row.toString() + "-" + col.toString());
+        currentTile.innerText = "";
+    }
+    else if (e.code === "Enter") {
+        update();
+    }
+
+    if (!gameOver && row === height) {
+        gameOver = true;
+        document.getElementById("answer").innerText = word;
+    }
 }
 
 function update() {
@@ -101,6 +147,11 @@ function update() {
         // Is it in the correct position?
         if (word[c] === letter) {
             currentTile.classList.add("correct");
+
+            let keyTile = document.getElementById("Key" + letter);
+            keyTile.classList.remove("present");
+            keyTile.classList.add("correct");
+
             correct += 1;
             letterCount[letter] -= 1;
         }
@@ -120,6 +171,10 @@ function update() {
             // Is it in the word?
             if (word.includes(letter) && letterCount[letter] > 0) {
                 currentTile.classList.add("present");
+                let keyTile = document.getElementById("Key" + letter);
+                if (!keyTile.classList.contains("correct")) {
+                    keyTile.classList.add("present");
+                }
                 letterCount[letter] -= 1;
             } // Not in the word
             else {
